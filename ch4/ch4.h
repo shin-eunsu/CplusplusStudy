0ee53a2325a8e62f0aef859cf5c0bead667a78ed
Eunsu <flost@naver.com>
2021-06-02 01:06:59 +0900

    21.06.01

--- ch4/ch4.h
+++ ch4/ch4.h
@@ -2,3 +2,4 @@
 int RectangleFaultMain();
 int Encaps1_Main();
 int Encaps2_Main();
+int Constructor1_Main();
--- ch4/main.cpp
+++ ch4/main.cpp
@@ -1,4 +1,5 @@
 #include "Q4_2.h"
+#include "ch4.h"
 
 int main(void)
 {
@@ -6,5 +7,8 @@ int main(void)
 	//Q4_1Main();
 	//Encaps1_Main();
 	//Encaps2_Main();
-	Q4_2Main();
+	//Q4_2Main();
+	//Constructor1_Main();
+
+
 }
